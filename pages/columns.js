2885bb8e7eb6a20e0faf9d98cfb14c9714ee4859
Mengsay LOEM <mengsayloem@Mengsays-MacBook-Air.local>
2023-06-23 14:57:10 +0900

    add pagination to News and Tips

--- pages/columns.js
+++ pages/columns.js
@@ -7,6 +7,12 @@ export default function Columns() {
   const [allColumnsData, setAllColumnsData] = useState([]);
   const [isFetching, setIsFetching] = useState(true);
 
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage] = useState(10); // Change this as per requirement
+
+  const [filterYear, setFilterYear] = useState("");
+  const [filterMonth, setFilterMonth] = useState("");
+
   const fetchAllColumns = async () => {
     setIsFetching(true);
     try {
@@ -19,6 +25,30 @@ export default function Columns() {
     }
     setIsFetching(false);
   };
+
+  // Get current posts
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  let currentPosts = allColumnsData.slice(indexOfFirstPost, indexOfLastPost);
+
+  // Change page
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
+  // Filter posts
+  currentPosts = currentPosts.filter((post) => {
+    const postDate = new Date(post.date);
+    return (
+      (filterYear ? postDate.getFullYear() === Number(filterYear) : true) &&
+      (filterMonth ? postDate.getMonth() + 1 === Number(filterMonth) : true)
+    );
+  });
+
+  // Generate year options dynamically
+  const currentYear = new Date().getFullYear();
+  const yearOptions = Array.from({ length: 5 }, (_, index) => currentYear - index); // past 5 years
+
+  // Generate month options dynamically
+  const monthOptions = Array.from({ length: 12 }, (_, index) => index + 1); // 12 months
 
   useEffect(() => {
     fetchAllColumns();
@@ -37,20 +67,44 @@ export default function Columns() {
         <h1 className=" text-5xl md:text-6xl lg:text-6xl font-bold mb-6 relative text-center text-blue-600">
           More From Us
         </h1>
-        <div className="  px-8 py-10 mx-auto lg:max-w-screen-xl sm:max-w-xl md:max-w-full sm:px-12 md:px-16 lg:py-20 sm:py-16">
+        <div className="px-8 py-10 mx-auto lg:max-w-screen-xl sm:max-w-xl md:max-w-full sm:px-12 md:px-16 lg:py-20 sm:py-16">
+          <div className="flex justify-center mb-4">
+            <div className="flex items-center bg-gray-100 rounded-lg p-2 space-x-2">
+              <label>Year:</label>
+              <select className="p-1 rounded bg-white" value={filterYear} onChange={(e) => setFilterYear(e.target.value)}>
+                <option value="">All</option>
+                {yearOptions.map((year) => (
+                  <option key={year} value={year}>{year}</option>
+                ))}
+              </select>
+
+              <label>Month:</label>
+              <select className="p-1 rounded bg-white" value={filterMonth} onChange={(e) => setFilterMonth(e.target.value)}>
+                <option value="">All</option>
+                {monthOptions.map((month) => (
+                  <option key={month} value={month}>{month}</option>
+                ))}
+              </select>
+              <button className="px-2 py-1 rounded bg-blue-600 text-white" onClick={() => paginate(1)}>Filter</button>
+            </div>
+          </div>
           <div className=" grid gap-x-8 gap-y-12 sm:gap-y-16 md:grid-cols-2 lg:grid-cols-3">
-          {allColumnsData.map((post) => (
-                <ColumnblockNotion
-                  key={post.id}
-                  post={{
-                    id: post.id,
-                    date: post.date,
-                    title: post.title,
-                    abstract: post.abstract,
-                    image: post.header_image,
-                  }}
-                />
-              ))}
+            {currentPosts.map((post) => (
+              <ColumnblockNotion
+                key={post.id}
+                post={{
+                  id: post.id,
+                  date: post.date,
+                  title: post.title,
+                  abstract: post.abstract,
+                  image: post.header_image,
+                }}
+              />
+            ))}
+          </div>
+          <div className="flex justify-center mt-4">
+            <button className="mx-1 px-3 py-2 bg-blue-600 text-white" onClick={() => paginate(currentPage > 1 ? currentPage - 1 : currentPage)}>Prev</button>
+            <button className="mx-1 px-3 py-2 bg-blue-600 text-white" onClick={() => paginate(currentPage < Math.ceil(allColumnsData.length / postsPerPage) ? currentPage + 1 : currentPage)}>Next</button>
           </div>
         </div>
       </section>
