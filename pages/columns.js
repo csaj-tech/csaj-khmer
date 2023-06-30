b334bb53f725c6015fbe26a31be1f95c1e3f1ec6
Mengsay Loem <loem.m.aa@m.titech.ac.jp>
2023-06-30 21:49:17 +0900

    fixed bug in filter

--- pages/columns.js
+++ pages/columns.js
@@ -9,7 +9,7 @@ export default function Columns() {
   const [oldestYear, setOldestYear] = useState((new Date()).getFullYear());
 
   const [currentPage, setCurrentPage] = useState(1);
-  const [postsPerPage] = useState(10);
+  const [postsPerPage] = useState(6);
 
   const [filterYear, setFilterYear] = useState("");
   const [filterMonth, setFilterMonth] = useState("");
@@ -32,22 +32,22 @@ export default function Columns() {
     setIsFetching(false);
   };
 
-  // Get current posts
-  const indexOfLastPost = currentPage * postsPerPage;
-  const indexOfFirstPost = indexOfLastPost - postsPerPage;
-  let currentPosts = allColumnsData.slice(indexOfFirstPost, indexOfLastPost);
-
-  // Change page
-  const paginate = (pageNumber) => setCurrentPage(pageNumber);
-
   // Filter posts
-  currentPosts = currentPosts.filter((post) => {
+  let filteredPosts = allColumnsData.filter((post) => {
     const postDate = new Date(post.date);
     return (
       (filterYear ? postDate.getFullYear() === Number(filterYear) : true) &&
       (filterMonth ? postDate.getMonth() + 1 === Number(filterMonth) : true)
     );
   });
+
+  // Get current posts
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  let currentPosts = filteredPosts.slice(indexOfFirstPost, indexOfLastPost);
+
+  // Change page
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
   // Generate year options dynamically
   const currentYear = new Date().getFullYear();
@@ -74,24 +74,26 @@ export default function Columns() {
           More From Us
         </h1>
         <div className="px-8 py-10 mx-auto lg:max-w-screen-xl sm:max-w-xl md:max-w-full sm:px-12 md:px-16 lg:py-20 sm:py-16">
-          <div className="flex justify-center mb-4">
-            <div className="flex items-center bg-gray-100 rounded-lg p-2 space-x-2">
-              <label>Year:</label>
-              <select className="p-1 rounded bg-white" value={filterYear} onChange={(e) => setFilterYear(e.target.value)}>
-                <option value="">All</option>
-                {yearOptions.map((year) => (
-                  <option key={year} value={year}>{year}</option>
-                ))}
-              </select>
+        <div className="flex justify-center mb-4 bg-gray-100 round-lg">
+            <div className="pl-3">
+              <div className="flex items-center bg-gray-100 rounded-lg p-2 space-x-2">
+                <label>Year:</label>
+                <select className="p-1 rounded bg-white" value={filterYear} onChange={(e) => setFilterYear(e.target.value)}>
+                  <option value="">All</option>
+                  {yearOptions.map((year) => (
+                    <option key={year} value={year}>{year}</option>
+                  ))}
+                </select>
 
-              <label>Month:</label>
-              <select className="p-1 rounded bg-white" value={filterMonth} onChange={(e) => setFilterMonth(e.target.value)}>
-                <option value="">All</option>
-                {monthOptions.map((month) => (
-                  <option key={month} value={month}>{month}</option>
-                ))}
-              </select>
-              <button className="px-2 py-1 rounded bg-blue-600 text-white" onClick={() => paginate(1)}>Filter</button>
+                <label>Month:</label>
+                <select className="p-1 rounded bg-white" value={filterMonth} onChange={(e) => setFilterMonth(e.target.value)}>
+                  <option value="">All</option>
+                  {monthOptions.map((month) => (
+                    <option key={month} value={month}>{month}</option>
+                  ))}
+                </select>
+
+              </div>
             </div>
           </div>
           <div className=" grid gap-x-8 gap-y-12 sm:gap-y-16 md:grid-cols-2 lg:grid-cols-3">
@@ -110,7 +112,7 @@ export default function Columns() {
           </div>
           <div className="flex justify-center mt-4">
             <button className="mx-1 px-3 py-2 bg-blue-600 text-white" onClick={() => paginate(currentPage > 1 ? currentPage - 1 : currentPage)}>Prev</button>
-            <button className="mx-1 px-3 py-2 bg-blue-600 text-white" onClick={() => paginate(currentPage < Math.ceil(allColumnsData.length / postsPerPage) ? currentPage + 1 : currentPage)}>Next</button>
+            <button className="mx-1 px-3 py-2 bg-blue-600 text-white" onClick={() => paginate(currentPage < Math.ceil(filteredPosts.length / postsPerPage) ? currentPage + 1 : currentPage)}>Next</button>
           </div>
         </div>
       </section>
