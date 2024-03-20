c2888bf7101614ae894bc86d312b8c1231a4e0b2
David Pich <davidpich.kh@gmail.com>
2024-03-20 16:48:01 +0900

    feat: refactor file structure to update about us page

---
+++ data/members/2024.js
@@ -0,0 +1,131 @@
+// TODO: CHANGE THE CONTENTS OF THIS FILE TO MATCH THE YEAR 2024
+// WHAT TO CHANGE: position, name, and imageUrl
+
+const leadingMembers = [
+  {
+    position: "Vice-President",
+    name: "Sreynet Han",
+    imageUrl: "/images/structures/2023/han_sreynet.png",
+  },
+  {
+    position: "President",
+    name: "Som Onn Ouch",
+    imageUrl: "/images/structures/2023/ouch_somonn.png",
+  },
+  {
+    position: "Vice-President",
+    name: "Sokuncharia Srey",
+    imageUrl: "/images/structures/2023/srey_sokuncharia.png",
+  },
+
+  // Add more leading members as needed
+];
+
+const regionalAssociations = [
+  {
+    position: "President of CSAK",
+    name: "Som Onn Ouch",
+    imageUrl: "/images/structures/2023/ouch_somonn.png",
+  },
+  {
+    position: "President of KSAK",
+    name: "Sathya Lim",
+    imageUrl: "/images/structures/2023/Sathya_Lim.jpg",
+  },
+  {
+    position: "President of CSAN",
+    name: "Chheav Reak Smey",
+    imageUrl: "/images/structures/2023/Chheav_Reak_Smey.jpg",
+  },
+  {
+    position: "President of ACSK",
+    name: "Tieng Rathanak",
+    imageUrl: "/images/structures/2023/Tieng_Rathanak.JPG",
+  },
+  {
+    position: "President of CSAH",
+    name: "Moeung Vanna",
+    imageUrl: "/images/structures/2023/Moeung_Vanna.JPG",
+  },
+  // Add more members as needed
+];
+
+const secretariatOfficers = [
+  {
+    position: "Secretary",
+    name: "Vouchleang Seng",
+    imageUrl: "/images/structures/2023/seng_vouchleang.png",
+  },
+  {
+    position: "Secretary",
+    name: "Yaly Sou",
+    imageUrl: "/images/structures/2023/sou_yaly.png",
+  },
+  {
+    position: "Secretary",
+    name: "Ponhahwatey Snameas",
+    imageUrl: "/images/structures/2023/snameas_ponhahwatey.png",
+  },
+  {
+    position: "Accountant",
+    name: "Sothida On",
+    imageUrl: "/images/structures/2023/on_sothida.png",
+  },
+  {
+    position: "Accountant",
+    name: "Manydine Ry",
+    imageUrl: "/images/structures/2023/ry_manydine.png",
+  },
+];
+
+const committeeChairs = [
+  {
+    position: "Chair of Cultural Committee",
+    name: "Pryalen Chhoeur",
+    imageUrl: "/images/structures/2023/chhoeur_pryalen.png",
+  },
+  {
+    position: "Chair of Educational Committee",
+    name: "Sothearoth Phou",
+    imageUrl: "/images/structures/2023/phou_sothearoth.png",
+  },
+  {
+    position: "Chair of Media and Communication Committee",
+    name: "Monyratana Mom",
+    imageUrl: "/images/structures/2023/mom_monyratana.png",
+  },
+  {
+    position: "Chair of Public Relation Committee",
+    name: "Oudom Oum",
+    imageUrl: "/images/structures/2023/oum_oudom.png",
+  },
+  {
+    position: "Chair of Student Service Committee",
+    name: "Vithyea Sam",
+    imageUrl: "/images/structures/2023/sam_vithyea.png",
+  },
+  {
+    position: "Chair of Event Planning Committee",
+    name: "Socheata Uk",
+    imageUrl: "/images/structures/2023/uk_socheata.png",
+  },
+  {
+    position: "Chair of Information Technology Committee",
+    name: "Mengsay Loem",
+    imageUrl: "/images/structures/2023/loem_mengsay.png",
+  },
+  {
+    position: "Chair of Administrative Committee",
+    name: "Sokleang Loch",
+    imageUrl: "/images/structures/2023/loch_sokleang.png",
+  },
+  // Add more committee chairs as needed
+];
+
+const memberlist = [
+  leadingMembers,
+  regionalAssociations,
+  secretariatOfficers,
+  committeeChairs,
+];
+export default memberlist;
