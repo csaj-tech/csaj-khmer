9765606f5cb90033b6c744e12b917726bf50c899
veyseng cheong <choeng.veyseng1@gmail.com>
2026-01-02 23:55:03 +0900

    Revert "[Refactor] Edited the font"
    This reverts commit be3afb7ed4dc043061d97b07d40c6abd74cd46c4.

--- app/layout.js
+++ app/layout.js
@@ -2,15 +2,7 @@ import "../styles/globals.css";
 import Navbar from "../components/navbar";
 import Footer from "../components/footer";
 import QueryProvider from "../components/queryprovider";
-import {Noto_Sans_Khmer } from "next/font/google";
-
-const font = Noto_Sans_Khmer({
-  subsets: ["latin"],
-  variable: "--font-lato",
-  weight: ["400", "700"],
-  display: "swap",
-});
-
+import { Open_Sans } from 'next/font/google'
 
 export const metadata = {
   title: "CSAJ",
@@ -19,8 +11,8 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className={`${font.variable}`}>
-      <body className="font-sans">
+    <html lang="en" className={openSans.className}>
+      <body>
         <QueryProvider>
           <div className="flex flex-col min-h-screen">
             <Navbar />
