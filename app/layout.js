2046541cf953eb9e8007243020bdc52d11639ad5
veyseng cheong <choeng.veyseng1@gmail.com>
2026-01-02 23:55:34 +0900

    [Refactor] Edited to noto sans khmer

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,15 @@ import "../styles/globals.css";
 import Navbar from "../components/navbar";
 import Footer from "../components/footer";
 import QueryProvider from "../components/queryprovider";
-import { Open_Sans } from 'next/font/google'
+import {Noto_Sans_Khmer } from "next/font/google";
+
+const font = Noto_Sans_Khmer({
+  subsets: ["latin"],
+  variable: "--font-lato",
+  weight: ["400", "700"],
+  display: "swap",
+});
+
 
 export const metadata = {
   title: "CSAJ",
@@ -11,8 +19,8 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className={openSans.className}>
-      <body>
+    <html lang="en" className={`${english.variable}`}>
+      <body className="font-sans">
         <QueryProvider>
           <div className="flex flex-col min-h-screen">
             <Navbar />
